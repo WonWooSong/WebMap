a040888bcf089d270d973fd2ba49ebfbf42aa181
wonwoo.song <songw@unbc.ca>
2020-03-03 00:33:29 -0800

    minor fixes and added another layer to map

--- map.py
+++ map.py
@@ -7,12 +7,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elevation = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif 1500 <= elevation < 3500:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lat,lon in zip(lat,lon,elevation):
-    fg.add_child(folium.Marker(location=[lat,lon], popup="This is Marker", icon=folium.Icon(color='green')))
+for lat,lon,elev in zip(lat,lon,elevation):
+    fg.add_child(folium.Marker(location=[lat,lon], popup=foliumfolium.Popup(str(elev), parse_html=True), icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
