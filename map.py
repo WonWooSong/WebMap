9a9304d4590a1239aead3cbc837080aeef002c8b
wonwoo.song <songw@unbc.ca>
2020-03-03 00:34:06 -0800

    minor fix

--- map.py
+++ map.py
@@ -20,7 +20,7 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lat,lon,elev in zip(lat,lon,elevation):
-    fg.add_child(folium.Marker(location=[lat,lon], popup=foliumfolium.Popup(str(elev), parse_html=True), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lat,lon], popup=folium.Popup(str(elev), parse_html=True), icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
