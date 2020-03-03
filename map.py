d444894e746eac5883a63b3751621560ac57cd6e
wonwoo.song <songw@unbc.ca>
2020-03-03 00:53:58 -0800

    differentiated population and volcano groups. Also, added layer control

--- map.py
+++ map.py
@@ -18,14 +18,19 @@ def color_producer(elevation):
 
 map = folium.Map(location=[40.0, -120.0], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-sig').read(), 
+fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 25000000
 else 'orange' if 25000000 <= x['properties']['POP2005'] < 55000000 else 'red'}))
 
 for lat,lon,elev,name in zip(lat,lon,elevation,name):
-    fg.add_child(folium.CircleMarker(location=[lat,lon], popup=folium.Popup(str(name), parse_html=True), radius = 10,
+    fgv.add_child(folium.CircleMarker(location=[lat,lon], popup=folium.Popup(str(name), parse_html=True), radius = 10,
     fill_color=color_producer(elev), fill=True,  color = 'grey', fill_opacity=0.7))
-map.add_child(fg)
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
