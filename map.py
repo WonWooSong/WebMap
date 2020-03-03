ebaad0edb5f088db9f79d23eb3026b839d7b2294
wonwoo.song <songw@unbc.ca>
2020-03-03 00:48:24 -0800

    changed pin style and popup info

--- map.py
+++ map.py
@@ -6,6 +6,7 @@ data = pandas.read_csv("data/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elevation = list(data["ELEV"])
+name = list(data["NAME"])
 
 def color_producer(elevation):
     if elevation < 1500:
@@ -20,8 +21,9 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-sig').read()))
 
-for lat,lon,elev in zip(lat,lon,elevation):
-    fg.add_child(folium.Marker(location=[lat,lon], popup=folium.Popup(str(elev), parse_html=True), icon=folium.Icon(color='green')))
+for lat,lon,elev,name in zip(lat,lon,elevation,name):
+    fg.add_child(folium.CircleMarker(location=[lat,lon], popup=folium.Popup(str(name), parse_html=True), radius = 10,
+    fill_color=color_producer(elev), fill=True,  color = 'grey', fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")
