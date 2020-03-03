a3587acda8e881e967a55928ad87600729c75eeb
wonwoo.song <songw@unbc.ca>
2020-03-02 23:00:57 -0800

    fixed data file name and using data in code.

--- map.py
+++ map.py
@@ -1,9 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="This is Marker", icon=folium.Icon(color='green')))
+
+for lat,lon in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lat,lon], popup="This is Marker", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
